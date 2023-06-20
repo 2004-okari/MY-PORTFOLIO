51c1fb62d92a07d118848e1397bc50e786c451d5
okari <nyandika15@gmail.com>
2023-06-20 09:52:26 +0300

    popup menu

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 let dropDown = document.getElementById("icon-1");
 let closeUp = document.getElementById("close");
 let items = document.querySelector('.img-1-nav');
+let closePort = document.querySelector(".refer-1");
+let closeAbout = document.querySelector(".refer-2");
+let closeCont = document.querySelector(".refer-3");
 
 let opening = () => {items.style.visibility = "visible"};
 let closing = () => {items.style.visibility = "hidden"};
 
 dropDown.addEventListener('click', opening);
 closeUp.addEventListener('click', closing);
+closePort.addEventListener('click', closing);
+closeAbout.addEventListener('click', closing);
+closeCont.addEventListener('click', closing);
