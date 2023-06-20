e729599c41ca1acb49fb7c84543e6b223821a49b
2004-okari <116469172+2004-okari@users.noreply.github.com>
2023-06-20 08:38:52 +0000

    Js file

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-let dropDown = document.getElementById("icon-1");
-let closeUp = document.getElementById("close");
-let items = document.querySelector('.img-1-nav');
-let closePort = document.querySelector(".refer-1");
-let closeAbout = document.querySelector(".refer-2");
-let closeCont = document.querySelector(".refer-3");
+const dropDown = document.getElementById('icon-1');
+const closeUp = document.getElementById('close');
+const items = document.querySelector('.img-1-nav');
+const closePort = document.querySelector('.refer-1');
+const closeAbout = document.querySelector('.refer-2');
+const closeCont = document.querySelector('.refer-3');
 
-let opening = () => {items.style.visibility = "visible"};
-let closing = () => {items.style.visibility = "hidden"};
+const opening = () => { items.style.visibility = 'visible'; };
+const closing = () => { items.style.visibility = 'hidden'; };
 
 dropDown.addEventListener('click', opening);
 closeUp.addEventListener('click', closing);
