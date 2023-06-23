bbbb2373727d580bb1fe8139d65a782ed9dbd2e3
2004-okari <116469172+2004-okari@users.noreply.github.com>
2023-06-23 18:33:10 +0300

    Contact validation (#13)
    * styling
    
    * js
    
    * Update index.js
    
    * js
    
    * js
    
    ---------
    
    Co-authored-by: okari <nyandika15@gmail.com>
    Co-authored-by: Nwanjoh Nalian Achei <88759996+NalianAchei2020@users.noreply.github.com>

--- index.js
+++ index.js
@@ -15,3 +15,67 @@ closeUp.addEventListener('click', closing);
 closePort.addEventListener('click', closing);
 closeAbout.addEventListener('click', closing);
 closeCont.addEventListener('click', closing);
+
+// Form Validation
+
+const nameError = document.querySelector('#name-err');
+const emailError = document.querySelector('#email-err');
+const textError = document.querySelector('#text-err');
+const submitError = document.querySelector('#submit-err');
+
+function validateName() {
+  const name = document.querySelector('#name-id').value;
+
+  if (name.length === 0) {
+    nameError.innerHTML = 'Name is required';
+    return false;
+  }
+
+  if (!name.match(/^[A-Za-z]*\s{1}[A-Za-z]*$/)) {
+    nameError.innerHTML = 'Wrie full name';
+    return false;
+  }
+  nameError.innerHTML = '<i class="fa-regular fa-circle-check" style="color: #3bc456;"></i>';
+  return true;
+}
+
+function validateEmail() {
+  const email = document.querySelector('#email-id').value;
+
+  if (email.length === 0) {
+    emailError.innerHTML = 'Email is required';
+    return false;
+  }
+
+  if (!email.match(/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$/)) {
+    emailError.innerHTML = 'Invalid email';
+    return false;
+  }
+
+  emailError.innerHTML = '<i class="fa-regular fa-circle-check" style="color: #3bc456;"></i>';
+  return true;
+}
+
+function validateText() {
+  const text = document.querySelector('#text-area').value;
+  const length = 30;
+  const required = length - text.length;
+
+  if (required > 0) {
+    textError.innerHTML = `${required}more characters left`;
+    return false;
+  }
+
+  textError.innerHTML = '<i class="fa-regular fa-circle-check" style="color: #3bc456;"></i>';
+  return true;
+}
+
+function validate() { // eslint-disable-line no-unused-vars
+  if (!validateName() || !validateEmail() || !validateText()) {
+    submitError.style.display = 'block';
+    submitError.innerHTML = 'Please fix error!';
+    setTimeout(() => { submitError.style.display = 'none'; }, 3000);
+    return false;
+  }
+  return true;
+}
